21c062a8394ae620b3622bf4b53af494e3570c04
mhdAzhar <azhartk20@gmail.com>
2022-12-10 06:26:33 +0700

    fix: fitur favorit

--- src/pages/Favorit/index.jsx
+++ src/pages/Favorit/index.jsx
@@ -1,24 +1,20 @@
 import { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
-import { getAllProduk } from '../../confiq/firebase'
+import { getUserById } from '../../confiq/firebase'
 import NavBarLogin from '../../components/NavBarLogin'
 import Loading from '../../components/Loading'
 import CardItem from '../../components/CardItem'
 import Footer from '../../components/Footer'
+import CONFIQ from '../../confiq/confiq'
 
 export default function Index ({ onLogout }) {
-  const { id } = useParams()
   const [loading, setLoading] = useState(false)
-  const [produk, setProduk] = useState([])
+  const [favorit, setFavorit] = useState([])
 
   useEffect(() => {
     async function getProduk () {
       setLoading(true)
-      const { error, AllProduk } = await getAllProduk()
-      if (!error) {
-        const produkPerKategori = AllProduk.filter((produk) => produk.kategori === id)
-        setProduk(produkPerKategori)
-      }
+      const { user } = await getUserById(localStorage.getItem(CONFIQ.authUser))
+      setFavorit(user.favorit)
       setLoading(false)
     }
     getProduk()
@@ -29,9 +25,9 @@ export default function Index ({ onLogout }) {
       <NavBarLogin logoutHandler={onLogout}/>
       <Loading visible={loading} />
       <div className='container mt-5 mb-5'>
-        <h1 className='mb-5'>kategori {id}</h1>
+        <h1 className='mb-5'>Produk Favorit mu</h1>
         <div className='row'>
-          {produk.length === 0 ? <div className='text-center text-bold text-secondary'>Tidak ada Produk yang sesuai.</div> : produk.map((item, index) => (<CardItem key={index} populer={item} {...item} />))}
+          {favorit.length === 0 ? <div className='text-center text-secondary'>Tidak ada Produk yang disukai.</div> : favorit.map((item, index) => (<CardItem key={index} populer={item} {...item} />))}
         </div>
       </div>
       <Footer />
